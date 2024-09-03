2a9e11867a0abcb955be2752bc8222d02805928b
Anil Nerurkar <anil.nerurkar@encora.com>
2024-09-03 10:33:31 +0530

    cosmetic changes in app.js and Task

--- taskmanager/src/main/java/codesynthesis/Task.java
+++ taskmanager/src/main/java/codesynthesis/Task.java
@@ -1,29 +1,35 @@
 package codesynthesis.encora.com.taskmanager;
 
+import java.util.Date;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.Objects;
 
 @Document("Tasks") // This will be the collection name in MongoDB
 public class Task {
 
     @Id
     private String id;
-    private String title;
+    private String name;
     private String description;
-    private boolean completed;
+    private Date dueDate;
+    private Date completionDate;
+    private boolean isComplete;
 
-    // Constructors 
+    // Constructors
     public Task() {
-        // Default constructor
+        // Default constructor for Spring Data MongoDB
     }
 
-    public Task(String title, String description, boolean completed) {
-        this.title = title;
+    public Task(String id, String name, String description, Date dueDate, Date completionDate, boolean isComplete) {
+        this.id = id;
+        this.name = name;
         this.description = description;
-        this.completed = completed;
+        this.dueDate = dueDate;
+        this.completionDate = completionDate;
+        this.isComplete = isComplete;
     }
-
-    // Getters and setters
+    
     public String getId() {
         return id;
     }
@@ -32,12 +38,12 @@ public class Task {
         this.id = id;
     }
 
-    public String getTitle() {
-        return title;
+    public String getName() {
+        return name;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getDescription() {
@@ -48,11 +54,52 @@ public class Task {
         this.description = description;
     }
 
-    public boolean isCompleted() {
-        return completed;
+    public Date getDueDate() {
+        return dueDate;
     }
 
-    public void setCompleted(boolean completed) {
-        this.completed = completed;
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Date getCompletionDate() {
+        return completionDate;
+    }
+
+    public void setCompletionDate(Date completionDate) {
+        this.completionDate = completionDate;
+    }
+
+    public boolean getIsComplete() {
+        return isComplete;
+    }
+
+    public void setIsComplete(boolean isComplete) {
+        this.isComplete = isComplete;
+    }
+
+    // hashCode and equals methods for proper object comparison
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, dueDate, completionDate, isComplete);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        Task other = (Task) obj;
+        return Objects.equals(id, other.id) && Objects.equals(name, other.name)
+                && Objects.equals(description, other.description) && Objects.equals(dueDate, other.dueDate)
+                && Objects.equals(completionDate, other.completionDate) && isComplete == other.isComplete;
+    }
+
+    // toString method for easy object representation
+    @Override
+    public String toString() {
+        return "Task [id=" + id + ", name=" + name + ", description=" + description + ", dueDate=" + dueDate
+                + ", completionDate=" + completionDate + ", isComplete=" + isComplete + "]";
     }
 }
